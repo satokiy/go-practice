b570aaac55580b2bd28e2b04a4136d94b6d6541e
satokiy <syamada@netprotections.co.jp>
2023-04-19 18:14:41 +0900

    電卓の作成

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
+
+
+func total(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return n + total(n-1)
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	l := widget.NewLabel("Hello Fyne!")
+	
+	e := widget.NewEntry()
+	e.SetText("0")
+	w.SetContent(
+		widget.NewVBox(
+			l,e,
+			widget.NewButton("Hi!", func() {
+				n, _ := strconv.Atoi(e.Text)
+				l.SetText("Total " + strconv.Itoa(total(n)) + "!")
+			}),
+		),
+	)
+	a.Settings().SetTheme(theme.DarkTheme())
 	w.ShowAndRun()
 }
