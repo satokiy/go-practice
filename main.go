4f5f272af1ea64cfb39348b478ff48e33be13e30
satokiy <syamada@netprotections.co.jp>
2023-04-19 14:17:44 +0900

    tmp.

--- main.go
+++ main.go
@@ -2,55 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sample/hello"
-	"strconv"
 )
+
+type Data interface{
+	Initial(name string, data []int)
+	PrintData()
+} 
 
 type MyData struct {
 	Name string
 	Data []int
 }
 
-type intp int
-
-func (num intp) IsPrime() bool {
-	for i := 2; i < int(num); i++ {
-		if int(num)%i == 0 {
-			return false
-		}
-	}
-	return true
+func (md *MyData) Initial(name string, data []int) {
+	md.Name = name
+	md.Data = data
 }
 
-// function which extract prime factor from number
-func (num intp) PrimeFactor() []int {
-	result := []int{}
-	x := int(num)
-
-	for i := 2; i < int(num); i++ {
-		if x%i == 0 {
-			x /= i
-			result = append(result, i)
-		}
-	}
-	return result
+func (md *MyData) Check() {
+	fmt.Printf("Check!! [%s]", md.Name)
 }
 
-func (num *intp) maxPrime() {
-	pf := num.PrimeFactor()
-	*num = intp(pf[len(pf)-1])
+// PrintData
+func (md *MyData) PrintData() {
+	fmt.Println("Name: ", md.Name)
+	fmt.Println("Data: ", md.Data)
 }
+
 
 func main() {
-	s := hello.Input("Type a number")
-	n, _ := strconv.Atoi(s)
-	x := intp(n)
-	fmt.Printf("%d is prime? %t.\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
-	x.maxPrime()
-	fmt.Printf("%d is prime? %t.\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
-	x++
-	fmt.Printf("%d is prime? %t.\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
+	var ob Data = new(MyData)
+	ob.Initial("Sachiko", []int{1,2,3})
+	ob.PrintData()
 }
